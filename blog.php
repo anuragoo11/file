dfce5c5ae13c42054fd46d2c929bc0a003c10fc5
anuragoo11 <156352223+anuragoo11@users.noreply.github.com>
2024-03-15 12:24:11 -0700

    Create blog.php

---
+++ blog.php
@@ -0,0 +1,109 @@
+<html lang="en">
+<head><style>
+.container {
+    display: flex;
+    height: 100vh; /* Full viewport height */
+}
+
+.fixed {
+    width: 100%;/* Half of the page width */
+    position: fixed;
+  
+  overflow: auto; /* Enable scrolling if content overflows */
+}
+        
+.title {
+    font-family:'Old English Text MT', serif;
+    color: red; 
+    text-align: center;
+    margin-top: 50px; 
+    background:linear-gradient(to right,#FFEB3B 25%, lightyellow 50%,#FFEB3B 100%);
+        }
+.subtitle {
+            font-family: 'Footlight MT Light'; 
+            color: #ffffff; /* White text color */
+            text-align: center;
+          background:linear-gradient(to right,black 0% ,red 50%,black 110%);
+            padding: 15px; /* Add padding to create space around the text */
+ border: 5px solid white;
+        }
+.navigation {
+    color: purple;
+    font-family: 'Lucida Calligraphy';/* Default font */
+    background:linear-gradient(to right, 
+            yellow 0%, 
+            yellow 14.28%, 
+            orange 28.56%, 
+            red 42.84%, 
+            orange 57.12%, 
+            yellow 71.4%, 
+            yellow 85.68%, 
+            yellow 100%
+        );
+    padding: 10px; /* Padding around the links */
+    size: 16px; /* Default font size */
+    border-radius: 5px;
+}
+.styled-heading {
+    color: red; /* Font color */
+    height:50px;
+    width:100px;
+    background: linear-gradient(235deg, orange, white);
+    font-family: 'Footlight MT Light'; /* Font family */
+    font-size: 36px; /* Font size */
+    padding: 10px; /* Padding around the heading */
+}
+.backgrounda{
+    border:3px solid yellow;
+    background: linear-gradient(135deg,purple,white,pink,white,purple);
+    
+}
+.fonta {
+    font-size:25px;
+    width:800px;
+    color:  #654321;/* Font color */
+    font-family: "Calisto MT", serif; /* Font family */
+}
+.backgroundb{
+    border: 2px solid red;
+  
+    background-color: yellow; /* Background color */
+}
+    </style>
+   
+</head>
+    <body>
+        <div class="backgrounda" class="container">
+        <div class="fixed">
+            <div class="backgroundb">
+            <br>
+                <header>
+                    <h1 class="title"><u>The care club</u></h1>
+                        <h2 class="subtitle">◦•●◉✿ CHARITY FOR ORPHANS ✿◉●•◦</h2>
+                            <nav>
+                                <div class="navigation">
+                                    <a href="checkout.php">Home</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="about.php">About</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="contact.php">Contact</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="blog.php"><b>◉ Blogs</b></a>
+                                </div>
+                            </nav>
+                </header>
+                <br>
+                </div>
+        </div>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <div >
+            <h1 class="styled-heading"><u>BLOGS</u></h1>
+                <p class="fonta"><b><u>Feeding Homeless Person_______</u></b><br>
+                Every day, we dedicate our efforts to reach out to homeless individuals in our community, offering them not just a meal, but a ray of hope. With your support, we can make a real difference in the lives of those facing the harsh realities of homelessness. Together, we can provide nourishment, comfort, and a renewed sense of dignity to those in need.<br><br><br>
+
+                <b><u>Help the helpless who need you______</u></b><br>
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p><br><br><br><br><br><br>
+
+
+
+
+
+
+
+        </div >
+    </body>
+</html>
