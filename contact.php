74903edb975a6d80f45fe2e5a0f25a25abaca1f0
anuragoo11 <156352223+anuragoo11@users.noreply.github.com>
2024-03-15 12:25:11 -0700

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,126 @@
+<html lang="en">
+<head>
+  <style>
+       .container {
+  display: flex;
+  height: 100vh; /* Full viewport height */
+}
+
+.fixed {
+   width: 100%;/* Half of the page width */
+  position: fixed;
+  
+  overflow: auto; /* Enable scrolling if content overflows */
+}
+
+        .title {
+            font-family: 'Old English Text MT', serif;
+            color: red;
+            text-align: center;
+            margin-top: 50px;
+  background:linear-gradient(to right,#FFEB3B 25%, lightyellow 50%,#FFEB3B 100%);
+        }
+.subtitle {
+            font-family: 'Footlight MT Light'; 
+            color: #ffffff; /* White text color */
+            text-align: center;
+             background:linear-gradient(to right,black 0% ,red 50%,black 110%);
+            padding: 15px; /* Add padding to create space around the text */
+ border: 5px solid white;
+        }
+.navigation {
+    color: purple;
+    font-family:'Lucida Calligraphy';
+    background:linear-gradient(to right, 
+            yellow 0%, 
+            orange 14.28%, 
+            red 28.56%, 
+            orange 42.84%, 
+            yellow 57.12%, 
+            yellow 71.4%, 
+            yellow 85.68%, 
+            yellow 100%
+        );
+    padding: 10px; /* Padding around the links */
+    size: 16px; /* Default font size */
+    border-radius: 5px;
+}
+.styled-heading {
+    color: red; /* Font color */
+    
+    font-family: 'Footlight MT Light'; /* Font family */
+    font-size: 36px; /* Font size */
+    padding: 10px; /* Padding around the heading */
+}
+.backgrounda{
+border:3px solid yellow;
+    background: linear-gradient(235deg,purple,white,pink,white,purple);
+}
+.fonta {
+    color: #654321; /* Font color */
+    font-family: "Calisto MT", serif; /* Font family */
+}
+
+.background-border {
+    color:lightgreen;
+    background: linear-gradient(135deg,white,black,black,black,black,white);
+    border: 2px solid #CCCCCC; /* Border style */
+    padding: 10px; /* Padding to keep the content away from the border */
+    background-clip: padding-box; /* Ensures that the background extends to the padding area */
+font-family: 'ALGERIAN'; 
+}
+
+.backgroundb{
+     border: 2px solid red;
+    background-color: yellow; /* Background color */
+}
+.circle {
+      width: 400px; /* Adjust the size of the circle */
+      height: 200px; /* Should be the same as width to maintain circular shape */
+       background: linear-gradient(135deg, pink,white,pink,purple);
+position : absolute;/* Set position to absolute *//* Shift square 50% down from the top */
+      left: 50%; /* Shift square 50% from the left */
+      transform: translate(-50%, 10%); /* Center the square */
+      
+     justify-content: center;
+      align-items: center;
+ border: 2px solid purple;
+    </style>
+</head>
+<body>
+<div class="backgrounda" class="container">
+<div class="fixed">
+<div class="backgroundb">
+<br>
+    <header>
+        <h1 class="title"><u>The care club</u></h1>
+        <h2 class="subtitle">◦•●◉✿ CHARITY FOR ORPHANS ✿◉●•◦</h2>
+        <nav>
+            <div class="navigation">
+               <a href="checkout.php">Home</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="about.php">About</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="contact.php"><b>◉ Contact</b></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="blog.php">Blogs</a>
+            </div>
+        </nav>
+    </header>
+<br>
+</div>
+</div>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<div class="fonta">
+<div class="circle">
+<br>
+<h1 class="styled-heading ">&nbsp;&nbsp;&nbsp;<u>CONTACT  DETAILS</u></h1>
+<ul>
+                <li><a href="https://www.instagram.com/anurag__oo11/">&nbsp;Instagram</a></li>
+                <li>&nbsp;call us at : 91+7709751308</li>
+</div>
+</div>
+            </ul><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+<div class="background-border ">
+<h1>&nbsp;&nbsp;&nbsp;Website created by Anurag Kharade | The Sparks Foundation 2024</h1><br><br>
+</div>
+
+    
+</div>
+</body>
+</html>
